a3f58b1aafc618cc1eaec7e77aa2599ecf13b3d1
sgandeplli <sgandepalli@softility.com>
2024-10-24 16:24:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   metadata = {
-    ssh-keys = "hr377:${file("/home/hr377/.ssh/id_rsa.pub")}"
+    ssh-keys = "root:${file("/root/.ssh/id_rsa.pub")}"
   }
 
   metadata_startup_script = <<-EOT
